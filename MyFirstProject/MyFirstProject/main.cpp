179c46fad713e77668e5f8388c4a387cb7395f7a
Carter Smith <124334987+CarterSmith318@users.noreply.github.com>
2023-09-11 15:49:39 -0500

    Add files via upload

---
+++ MyFirstProject/MyFirstProject/main.cpp
@@ -0,0 +1,27 @@
+/*
+	Carter Smith
+	C++ 2023
+	Due: September 15, 2023
+	Lab 1
+
+	This lab will serve as a demonstration of your ability to download a project from
+	GitHub, compile and run the project, upload a project to GitHub, and submit a project to Canvas.
+	This lab assumes you have downloaded and installed an IDE (integrated development
+	environment) and have also installed GitHub Desktop. If you have not met the assumptions for
+	the lab, the steps you need to complete the lab may vary from what is here. The goals are for
+	you to download a project, compile and run that project, and to upload your own project. If you
+	meet the goals, you will get the points for this lab. Follow the directions in this lab and complete
+	the tasks.
+
+*/
+
+#include <iostream>
+
+int main() {
+
+	std::cout << "Hello World!\n";
+	std::cout << "My name is Carter Smith\n";
+	std::cout << "My favorite color is red\n";
+
+	return 0;	
+}
